745d138c45a460ca399e265b8889ba368419ad49
Mono <quentinguillemineau@gmail.com>
2023-12-20 20:12:55 +0100

    add quote but WIP

--- about_us.php
+++ about_us.php
@@ -24,13 +24,15 @@
         </figure>
         <article>
             <img id="profil" src="assets/img/Lucas.jpg" alt="Lucas Boillot">
+            <img id="firstquote" src="assets/img/quote.svg" alt="quote">
             <aside>
                 <section>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper.
                 </section>
             </aside>
+            <img id="endquote" src="assets/img/entypo_quote.svg" alt="endquote">
             <h4>Lucas BOILLOT</h4>
-            <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Site</a>
+            <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
             <a id="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
         </article>
@@ -38,11 +40,13 @@
             <img id="profil" src="assets/img/Lucas.jpg" alt="Lucas Boillot">
             <aside>
                 <section>
+                    <img src="assets/img/quote.svg" alt="quote">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper.
+                    <img src="assets/img/entypo_quote.svg" alt="endquote">
                 </section>
             </aside>
             <h4>Lucas BOILLOT</h4>
-            <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Site</a>
+            <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
             <a id="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
         </article>
@@ -50,11 +54,13 @@
             <img id="profil" src="assets/img/Lucas.jpg" alt="Lucas Boillot">
             <aside>
                 <section>
+                    <img src="assets/img/quote.svg" alt="quote">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper.
+                    <img src="assets/img/entypo_quote.svg" alt="endquote">
                 </section>
             </aside>
             <h4>Lucas BOILLOT</h4>
-            <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Site</a>
+            <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
             <a id="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
         </article>
