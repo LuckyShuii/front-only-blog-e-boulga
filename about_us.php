c33242e1ad9e70c00b21a913372c8bf1e3f04393
Mono <quentinguillemineau@gmail.com>
2023-12-20 20:34:36 +0100

    little improvement about quote

--- about_us.php
+++ about_us.php
@@ -30,7 +30,9 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper.
                 </section>
             </aside>
-            <img id="endquote" src="assets/img/entypo_quote.svg" alt="endquote">
+            <figure id="endquote">
+                <img src="assets/img/entypo_quote.svg" alt="endquote">
+            </figure>
             <h4>Lucas BOILLOT</h4>
             <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
