a40d6ea3174651c176ef1a7862e47a8df8ab6627
LuckyShuii <lucasnimes30000@gmail.com>
2023-12-20 19:28:26 +0100

    header & footer done with responsive and burger

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,68 @@
 let logoHeader = document.querySelector("#logo_header")
+let myHeader = document.querySelector("header")
+let burgerClosedIcon = document.querySelector("#closed_burger_icon")
+let myFooter = document.querySelector("footer")
+let burgerMenue = document.querySelector("#burger_menue")
+let burgerOpennedIcon = document.querySelector("#openned_burger_icon")
+let myMain = document.querySelector("main")
 
-logoHeader.addEventListener("click", () => {
-    window.location = "../index.php"
-})
+let myMedia = window.matchMedia("(min-width: 1090px)")
+
+function onload() {
+    logoHeader.addEventListener("click", () => {
+        window.location = "../index.php"
+    })
+
+    burgerClosedIcon.style.cursor = "pointer"
+    burgerOpennedIcon.style.cursor = "pointer"
+
+    burgerClosedIcon.addEventListener("click", () => {
+        openingBurger()
+    })
+
+    burgerOpennedIcon.addEventListener("click", () => {
+        closingBurger()
+    })
+
+    myMediaQuerry(myMedia)
+
+    myMedia.addEventListener('change', () => {
+        myMediaQuerry(myMedia)
+    });
+}
+
+function myMediaQuerry(myMedia) {
+    if (myMedia.matches) {
+        burgerOpennedIcon.style.display = "none"
+        burgerMenue.style.display = "none"
+        myFooter.style.display = "flex"
+        myMain.style.display = "block"
+    } else {
+        burgerClosedIcon.style.display = "flex"
+    }
+}
+
+function openingBurger() {
+    burgerMenue.style.display = "flex"
+    burgerClosedIcon.style.display = "none"
+    burgerOpennedIcon.style.display = "block"
+    burgerMenue.style.animation = "slide-left .5s ease-in both"
+    myFooter.style.display = "none"
+    myHeader.style.borderRadius = "0"
+    myMain.style.display = "none"
+}
+
+function closingBurger() {
+    myFooter.style.display = "flex"
+    burgerClosedIcon.style.display = "block"
+    burgerOpennedIcon.style.display = "none"
+    burgerMenue.style.animation = "slide-right .5s ease-out both"
+    setTimeout(() => {
+        burgerMenue.style.display = "none"
+    }, 500);
+    myHeader.style.borderRadius = "0 0 1rem 1rem"
+    myMain.style.display = "block"
+
+}
+
+onload()
--- header.php
+++ header.php
@@ -4,10 +4,21 @@
     <ul id="list_nav_header">
         <li><a href="#"><img src="assets/img/home.svg" alt="icône d'une maison" /> Accueil</a></li>
         <li><a href="#"><img src="assets/img/buste.svg" alt="icône d'un buste d'homme" /> Qui sommes-nous ?</a></li>
-        <li><a href="#"><img src="assets/img/enveloppe.svg" alt="icône d'une enveloppe" /> Contactez-nous ?</a></li>
+        <li><a href="#"><img src="assets/img/enveloppe.svg" alt="icône d'une enveloppe" /> Contactez-nous</a></li>
     </ul>
 
     <section id="burger_icon">
-
+        <img id="closed_burger_icon" src="assets/img/closed-burger.svg" alt="logo du menu burger fermé, 3 lignes superposées horizontales" />
+        <img id="openned_burger_icon" src="assets/img/openned-burger.svg" alt="logo du menu burger ouvert, une croix" />
     </section>
 </header>
+
+<ul id="burger_menue">
+    <hr class="burger_separation_header">
+    <li><a href="#"><img src="assets/img/home.svg" alt="icône d'une maison" /> Accueil</a></li>
+    <hr class="burger_separation">
+    <li><a href="#"><img src="assets/img/buste.svg" alt="icône d'un buste d'homme" /> Qui sommes-nous ?</a></li>
+    <hr class="burger_separation">
+    <li><a href="#"><img src="assets/img/enveloppe.svg" alt="icône d'une enveloppe" /> Contactez-nous</a></li>
+    <hr class="burger_separation">
+</ul>
