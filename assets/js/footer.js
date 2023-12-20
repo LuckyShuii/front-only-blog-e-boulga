fcd741f4e9f2592baa346a537a744ec38e73417c
LuckyShuii <lucasnimes30000@gmail.com>
2023-12-20 14:21:49 +0100

    added cursor pointer on hover, on socials

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,6 +3,7 @@ let socials = document.querySelectorAll(".socials_footer")
 // boucle qui attribue une redirection vers le lien adéquat en cliquant sur les logos de réseaux sociaux
 
 socials.forEach(social => {
+    social.style.cursor = "pointer"
     social.addEventListener("click", () => {
         switch (social.id) {
             case 'tiktok_logo':
