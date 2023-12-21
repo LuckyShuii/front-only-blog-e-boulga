447f1c9ee0482418a7b1eb72097ba22fcfbbfb41
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2023-12-21 13:37:58 +0100

    big card done

--- home.php
+++ home.php
@@ -1,12 +1,26 @@
-<section class = hotNews_block>
-    <div class="bigNews">
-        csdcs
-        <div class="title_bigNews title_news" >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea saepe sunt quia rerum voluptatem maxime dolores error laudantium pariatur quam.
+<section class=hotNews_block>
+    <article class="card bigNews">
+        <div class="title_bigNews title_news">
+            <img class="big" src="../assets/img/home_picture/montagne1.png" alt="image de l'article principal">
+            <div class="articleTeasing">
+                <h1>Lorem ipsum dolor sit amet.</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur laboriosam numquam ducimus iste reprehenderit illum deleniti sequi, vitae voluptates, molestiae facere eligendi fuga, ratione quia animi debitis saepe perspiciatis libero?</p>
+            </div>
         </div>
-    </div>
-    <div class="middle_news">
 
-    </div>
+    </article>
+
+    <article class="card middle_news">
+        <div class="card1">
+            <img class="small" src="../assets/img/home_picture/montagne1.png" alt="image de l'article principal">
+
+            <h1>Lorem ipsum dolor sit amet.
+            </h1>
+
+        </div>
+        <div class="card1">
+
+        </div>
+    </article>
+
 </section>
-
