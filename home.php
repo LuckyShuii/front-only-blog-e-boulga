ce79f9a02a67640851fa1b8cd4c901ff7572e611
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2023-12-21 18:14:41 +0100

    middle_news WIP

--- home.php
+++ home.php
@@ -1,30 +1,94 @@
-<section class=hotNews_block>
-    <article class="card bigNews">
-        <a href="#">
-            <div class="title_bigNews title_news">
-                <img class="big" src="../assets/img/home_picture/montagne1.png" alt="image de l'article principal">
-                <div class="articleTeasing">
-                    <h1>Lorem ipsum dolor sit amet.</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur laboriosam numquam ducimus iste reprehenderit illum deleniti sequi, vitae voluptates, molestiae facere eligendi fuga, ratione quia animi debitis saepe perspiciatis libero?</p>
+    <section class=hotNews_block>
+        <article class="card bigNews">
+            <a href="#">
+                <div class="title_bigNews title_news">
+                    <img class="big" src="../assets/img/home_picture/montagne1.png" alt="image de l'article principal">
+                    <div class="articleTeasing">
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur laboriosam numquam ducimus iste reprehenderit illum deleniti sequi, vitae voluptates, molestiae facere eligendi fuga, ratione quia animi debitis saepe perspiciatis libero?</p>
+                    </div>
+                </div>
+            </a>
+        </article>
+
+        <article class="card middle_news">
+            <a href="#">
+                <div class="card1">
+                    <img class="small" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="card1">
+                    <img class="small" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                </div>
+            </a>
+        </article>
+    </section>
+
+    <span class="separation"><img src="../assets/img/separation.svg" alt="Barre de séparation"></span>
+
+    <section class="article_list">
+        <article class="article_block">
+            <div class="card_container">
+                <div class="card2">
+                    <img class="medium" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
                 </div>
             </div>
-        </a>
-    </article>
+            <div class="little_article">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos consequatur exercitationem, autem pariatur expedita nesciunt. Nostrum dolorem sequi, amet pariatur soluta provident corporis, illo ea inventore natus quo velit vero nesciunt fugiat harum aliquid modi impedit autem veniam facere ipsam. Neque quaerat pariatur unde earum, libero impedit quo soluta architecto, perferendis a sed, quas fugit doloremque cupiditate facere modi officiis.
 
-    <article class="card middle_news">
-        <a href="#">
-        <div class="card1">
-                <img class="small" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
-                <h2>Lorem ipsum dolor sit amet.</h2>    
+                </p>
             </div>
-        </a>
-        
-        <a href="#">
-        <div class="card1">
-                <img class="small" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
-                <h2>Lorem ipsum dolor sit amet.</h2>
-            </div>
-        </a>
-    </article>
+        </article>
 
-</section>
+        <article class="article_block">
+            <div class="card2">
+                <img class="medium" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
+            </div>
+            <div class="little_article">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos consequatur exercitationem, autem pariatur expedita nesciunt. Nostrum dolorem sequi, amet pariatur soluta provident corporis, illo ea inventore natus quo velit vero nesciunt fugiat harum aliquid modi impedit autem veniam facere ipsam. Neque quaerat pariatur unde earum, libero impedit quo soluta architecto, perferendis a sed, quas fugit doloremque cupiditate facere modi officiis.
+
+                </p>
+            </div>
+        </article>
+
+        <article class="article_block">
+            <div class="card2">
+                <img class="medium" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
+            </div>
+            <div class="little_article">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos consequatur exercitationem, autem pariatur expedita nesciunt. Nostrum dolorem sequi, amet pariatur soluta provident corporis, illo ea inventore natus quo velit vero nesciunt fugiat harum aliquid modi impedit autem veniam facere ipsam. Neque quaerat pariatur unde earum, libero impedit quo soluta architecto, perferendis a sed, quas fugit doloremque cupiditate facere modi officiis.
+
+                </p>
+            </div>
+        </article>
+
+        <article class="article_block">
+            <div class="card2">
+                <img class="medium" src="../assets/img/home_picture/montagne1.png" alt="image de l'article secondaire">
+            </div>
+            <div class="little_article">
+                <h3>Lorem ipsum dolor sit amet.</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos consequatur exercitationem, autem pariatur expedita nesciunt. Nostrum dolorem sequi, amet pariatur soluta provident corporis, illo ea inventore natus quo velit vero nesciunt fugiat harum aliquid modi impedit autem veniam facere ipsam. Neque quaerat pariatur unde earum, libero impedit quo soluta architecto, perferendis a sed, quas fugit doloremque cupiditate facere modi officiis.
+
+                </p>
+            </div>
+        </article>
+
+
+
+
+
+
+
+
+
+
+
+    </section>
