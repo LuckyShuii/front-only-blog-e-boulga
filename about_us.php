22d2b78701aa839f2fa2c5235b712018093adcc8
Mono <quentinguillemineau@gmail.com>
2023-12-21 11:01:23 +0100

    modification of h4 in h2

--- about_us.php
+++ about_us.php
@@ -33,35 +33,39 @@
             <!-- <figure id="endquote">
                 <img src="assets/img/entypo_quote.svg" alt="endquote">
             </figure> -->
-            <h4>Lucas BOILLOT</h4>
+            <h2>Lucas BOILLOT</h2>
             <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
             <a id="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
         </article>
         <article>
             <img id="profil" src="assets/img/Lucas.jpg" alt="Lucas Boillot">
+            <img id="firstquote" src="assets/img/quote.svg" alt="quote">
             <aside>
                 <section>
-                    <img src="assets/img/quote.svg" alt="quote">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper.
-                    <img src="assets/img/entypo_quote.svg" alt="endquote">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. <span> <img src="assets/img/entypo_quote.svg" alt="endquote"> </span>
                 </section>
             </aside>
-            <h4>Lucas BOILLOT</h4>
+            <!-- <figure id="endquote">
+                <img src="assets/img/entypo_quote.svg" alt="endquote">
+            </figure> -->
+            <h2>Lucas BOILLOT</h2>
             <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
             <a id="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
         </article>
         <article>
             <img id="profil" src="assets/img/Lucas.jpg" alt="Lucas Boillot">
+            <img id="firstquote" src="assets/img/quote.svg" alt="quote">
             <aside>
                 <section>
-                    <img src="assets/img/quote.svg" alt="quote">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper.
-                    <img src="assets/img/entypo_quote.svg" alt="endquote">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. <span> <img src="assets/img/entypo_quote.svg" alt="endquote"> </span>
                 </section>
             </aside>
-            <h4>Lucas BOILLOT</h4>
+            <!-- <figure id="endquote">
+                <img src="assets/img/entypo_quote.svg" alt="endquote">
+            </figure> -->
+            <h2>Lucas BOILLOT</h2>
             <a id="website" href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
             <a id="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
             <a id="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
