b0b3d22578fc1a4d503774ebd80b0b4dce393bcb
LuckyShuii <lucasnimes30000@gmail.com>
2023-12-22 14:27:24 +0100

    work in progress burger

--- assets/js/header.js
+++ assets/js/header.js
@@ -2,39 +2,34 @@ let logoHeader = document.querySelector("#logo_header")
 let myHeader = document.querySelector("header")
 let burgerClosedIcon = document.querySelector("#closed_burger_icon")
 let myFooter = document.querySelector("footer")
-let burgerMenue = document.querySelector("#burger_menue")
+let burgerMenue = document.querySelector("#burger_icon")
 let burgerOpennedIcon = document.querySelector("#openned_burger_icon")
 let myMain = document.querySelector("main")
 
-/**
- * Appel de la fonction onload() qui vient charger les écouteurs d'évenements
- */
 window.onload = onload();
 
-/**
- * Fonction appelée au chargement de la page avec toutes les fonctionnalités et écouteurs d'évenements
- */
 function onload() {
 
-    /**
-     * / Variable mediaquerry pour le menu burger
-     */
+    burgerMenue.addEventListener("click", () => {
+        burgerMenue.classList.toggle('opened');
+        burgerMenue.setAttribute('aria-expanded', burgerMenue.classList.contains('opened'))
+        console.warn(burgerMenue.classList[0])
+        switch (burgerMenue.classList[2]) {
+            case "undisplayed":
+                console.warn("undisplayed")
+                openingBurger()
+                break;
+            case "isFlexed":
+                console.warn("isFlexed")
+                closingBurger()
+                break;
+        }
+    })
+
     let myMedia = window.matchMedia("(min-width: 1090px)")
 
     logoHeader.addEventListener("click", () => {
         window.location = "../index.php"
-    })
-
-    logoHeader.style.cursor = "pointer"
-    burgerClosedIcon.style.cursor = "pointer"
-    burgerOpennedIcon.style.cursor = "pointer"
-
-    burgerClosedIcon.addEventListener("click", () => {
-        openingBurger()
-    })
-
-    burgerOpennedIcon.addEventListener("click", () => {
-        closingBurger()
     })
 
     myMediaQuerry(myMedia)
@@ -44,44 +39,33 @@ function onload() {
     });
 }
 
-/**
- * Fonction qui permet de fermer le menu burger si il est ouvert et que la fenêtre s'agrandit 
- */
 function myMediaQuerry(myMedia) {
     if (myMedia.matches) {
-        burgerOpennedIcon.style.display = "none"
-        burgerMenue.style.display = "none"
-        myFooter.style.display = "flex"
-        myMain.style.display = "block"
+        // burgerOpennedIcon.classList.add('undisplayed')
+        burgerMenue.classList.add('undisplayed')
+        myFooter.classList.toggle('isFlexed')
+        myMain.classList.toggle('asBlock')
     } else {
-        burgerClosedIcon.style.display = "flex"
+        burgerMenue.classList.remove('undisplayed')
     }
 }
 
-/**
- * Fonction qui se déclenche à l'ouverture du menu burger
- */
 function openingBurger() {
-    burgerMenue.style.display = "flex"
-    burgerClosedIcon.style.display = "none"
-    burgerOpennedIcon.style.display = "block"
-    burgerMenue.style.animation = "slide-left .5s ease-in both"
-    myFooter.style.display = "none"
-    myHeader.style.borderRadius = "0"
-    myMain.style.display = "none"
+    burgerMenue.classList.toggle('isFlexed')
+    // burgerClosedIcon.classList.toggle('undisplayed')
+    burgerOpennedIcon.classList.toggle('asBlock')
+    burgerMenue.classList.toggle('easeOutAnimation')
+    myFooter.classList.toggle('undisplayed')
+    myMain.classList.toggle('undisplayed')
 }
-
-/**
- * Fonction qui se déclenche à la fermeture du menu burger
- */
 function closingBurger() {
-    myFooter.style.display = "flex"
-    burgerClosedIcon.style.display = "block"
-    burgerOpennedIcon.style.display = "none"
-    burgerMenue.style.animation = "slide-right .5s ease-out both"
+    myFooter.classList.toggle('isFlexed')
+    // burgerClosedIcon.classList.toggle('asBlock')
+    burgerOpennedIcon.classList.toggle('undisplayed')
+    burgerMenue.classList.toggle('easeOutAnimation')
     setTimeout(() => {
-        burgerMenue.style.display = "none"
+        burgerMenue.classList.toggle('undisplayed')
     }, 500);
-    myMain.style.display = "block"
+    myMain.classList.toggle('asBlock')
 
 }
