c631d09d6e01bf8ebf7f729d500e28f5ae2f36b2
Mono <quentinguillemineau@gmail.com>
2024-01-03 12:27:41 +0100

    edit about_us with json data

--- about_us.php
+++ about_us.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="UTF-8">
@@ -13,81 +13,74 @@
     <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@200&display=swap" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="assets/img/logo_fav.svg">
     <link rel="mask-icon" href="assets/img/logo_fav.svg">
-    <title>Accueil</title>
 </head>
-
-<?php require_once('header.php'); ?>
+<?php
+require_once('header.php'); ?>
 
 <body>
     <main>
         <h1 id="pageTitle">L’équipe Blog-e-Boulga</h1>
         <p id="pagePresentation">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis. <br /><br />Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat.
+            Bienvenue dans notre équipe de développement web, la diversité de nos parcours professionnels se conjugue pour créer une synergie dans la réalisation de notre projet commun : un blog dédié au développement web.<br>
+            Notre objectif commun est de faire de cet espace une ressource pour les passionnés de développement web, des débutants aux professionnels chevronnés. Nous croyons que notre collaboration et nos perspectives variées contribueront à faire de ce projet une référence dans l'univers du développement web.
         </p>
         <figure class="separationAbout">
             <img class="longSeparation" src="assets/img/separation.svg" alt="separation">
             <img class="smallSeparation" src="assets/img/small_separation.svg" alt="small separation">
         </figure>
-        <article>
-            <figure class="profil">
-                <img src="assets/img/Lucas2.jpg" alt="Lucas Boillot">
-            </figure>
-            <img class="firstquote" src="assets/img/quote.svg" alt="quote">
-            <aside>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. <span> <img src="assets/img/entypo_quote.svg" alt="endquote"> </span>
-                </p>
-            </aside>
-            <h2>Lucas BOILLOT</h2>
-            <div class="website">
-                <a href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
-            </div>
-            <a class="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
-            <a class="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
-        </article>
-        <figure class="separationAbout">
-            <img class="smallSeparation" src="assets/img/small_separation.svg" alt="small separation">
-        </figure>
-        <article>
-            <figure class="profil">
-                <img src="assets/img/Lucas2.jpg" alt="Lucas Boillot">
-            </figure>
-            <img class="firstquote" src="assets/img/quote.svg" alt="quote">
-            <aside>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra sempera. <span> <img src="assets/img/entypo_quote.svg" alt="endquote"> </span>
-                </p>
-            </aside>
-            <h2>Lucas BOILLOT</h2>
-            <div class="website">
-                <a href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
-            </div>
-            <a class="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
-            <a class="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
-        </article>
-        <figure class="separationAbout">
-            <img class="smallSeparation" src="assets/img/small_separation.svg" alt="small separation">
-        </figure>
-        <article>
-            <figure class="profil">
-                <img src="assets/img/Lucas2.jpg" alt="Lucas Boillot">
-            </figure>
-            <img class="firstquote" src="assets/img/quote.svg" alt="quote">
-            <aside>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum facilisis massa nec maximus. Ut convallis fringilla finibus. Praesent convallis placerat risus. Morbi luctus commodo magna rhoncus lobortis. Morbi dignissim id ante a imperdiet. Nam elementum lorem quis semam.<br /><br /> consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. Praesent congue ornare lorem, non mollis est mollis non. Curabitur urna sapien, feugiat vel consequat eu, elementum non enim. Morbi vel mauris metus. Maecenas consectetur congue turpis eu gravida. Integer imperdiet tincidunt tortor. Pellentesque gravida lorem vel luctus convallis. Aliquam erat volutpat. Ut consectetur urna quis condimentum elementum. Aenean consectetur nisl ut velit viverra semper. <span> <img src="assets/img/entypo_quote.svg" alt="endquote"> </span>
-                </p>
-            </aside>
-            <h2>Lucas BOILLOT</h2>
-            <div class="website">
-                <a href="#"><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
-            </div>
-            <a class="linkedinProfil" href="#"><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
-            <a class="githubProfil" href="#"><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
-        </article>
+
+        <?php
+        require_once('header.php');
+        /**
+         * Récupération du contenu des fichier json articles & authors (future database)
+         */
+        (array) $json_authors = file_get_contents("assets/db/db_authors.json");
+        (array) $parsed_authors = json_decode($json_authors);
+
+        /**
+         * Check if le contenu récupéré est bien du json, sinon renvoyer une erreur
+         */
+        if (json_validate($json_authors)) {
+            /**
+             * @param array $authors = Stockage du tableau/index "authors" afin de le renvoyer dans la fonction pour itérer dedans
+             */
+            (array) $authors = $parsed_authors->authors;
+            //var_dump($authors);
+            foreach ($authors as $author) {
+                echo '
+                    <article>
+                    <div class="leftSide">
+                        <figure class="profil">
+                            <img src="assets/img/' . $author->img . '" alt="Lucas Boillot">
+                        </figure>
+                        <span class="authorName">
+                        <h2>' . $author->firstName . ' ' . $author->lastName . '</h2>
+                        </span>
+                        <div class="website">
+                            <a href=' . $author->website . '><img src="assets/img/myWebSite.svg" alt="Site web personnel"> Mon Site</a>
+                        </div>
+                        </div>
+                        <img class="firstquote" src="assets/img/quote.svg" alt="quote">
+                        <aside>
+                            <p>
+                                ' . $author->fullDescription . ' <span> <img src="assets/img/entypo_quote.svg" alt="endquote"> </span>
+                            </p>
+                        </aside>
+                        <a class="linkedinProfil" href=' . $author->linkedin . '><img src="assets/img/linkedin.svg" alt="Profil Linkedin"> Mon profil Linkedin</a>
+                        <a class="githubProfil" href=' . $author->gitHub . '><img src="assets/img/github.svg" alt="Profil Github"> Mon espace Github</a>
+                    </article>
+                    <figure class="separationAbout">
+                                <img class="smallSeparation" src="assets/img/small_separation.svg" alt="small separation">
+                            </figure>';
+            }
+        } else {
+            /**
+             * Erreur renvoyée si le contenu n'est pas du JSON (fichier mal formé)
+             */
+            throw new Exception("Une erreur s'est produite : Le contenu récupéré n'est pas au format JSON !");
+        }
+        ?>
     </main>
-    <script src="assets/js/header.js" defer></script>
-    <script src="assets/js/footer.js" defer></script>
 </body>
 <?php require_once('footer.php'); ?>
 
